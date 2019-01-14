001f4a53b56fe714bad16a02ecbb72978c05baae
Vangel Paronov <vangel@WhiteFrog.local>
2019-01-14 19:58:04 +0100

    checkpoint

--- App.js
+++ App.js
@@ -7,16 +7,19 @@
  */
 
 import React, { Component } from 'react';
-//import { Platform, StyleSheet, Text, View } from 'react-native';
-import Header from './src/components/header';
+import { View } from 'react-native';
+import Header from './src/components/Header';
+import AlbumList from './src/components/AlbumList';
 
 
 type Props = {};
 export default class App extends Component<Props> {
   render() {
     return (
-      <Header />
-    );
+      <View>
+        <Header headerText={'Albums'} />
+        <AlbumList />
+      </View>    
+  );
   }
 }
-
